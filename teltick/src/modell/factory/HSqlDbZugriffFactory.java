1a06fbea33a7b52f667c8c3ecb59d36649bcf990
benedikt <benedikt@benedikt-Aspire-M5811>
2015-11-15 21:35:02 +0100

    Versuch Fehlerbehebung DB

--- teltick/src/modell/factory/HSqlDbZugriffFactory.java
+++ teltick/src/modell/factory/HSqlDbZugriffFactory.java
@@ -4,12 +4,9 @@ import modell.implementierung.ImpHSqlDbZugriff;
 import modell.interfaces.DBZugriff;
 
 public class HSqlDbZugriffFactory {
-
-	private static DBZugriff obj = null;
 	
 	public static DBZugriff getInstance(){
-		if ( obj == null) obj = new ImpHSqlDbZugriff();;
-		return obj;
+		return new ImpHSqlDbZugriff();
 	}
 
 }
