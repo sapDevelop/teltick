704a789ce5748dfb887dcf5c3ced2f9327c3a41b
Robin <Robin@Y50.fritz.box>
2015-11-18 19:50:51 +0100

    Kleines Bugfixing!

--- teltick/src/controller/LogoutController.java
+++ teltick/src/controller/LogoutController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,38 +22,39 @@ import modell.entitaeten.interfaces.Mitarbeiter;
 @WebServlet("/LogoutController")
 public class LogoutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-      
+
 	private static Logger log = LogFactory.getInstance(LogoutController.class.getName());
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LogoutController() {
-        super();
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LogoutController() {
+		super();
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		log.info("LogoutController aufgerufen");
-		
+
 		HttpSession session = request.getSession();
 		Mitarbeiter m = (Mitarbeiter) session.getAttribute("angemeldeterMitarbeiter");
-		
+
 		if (m != null){
 			m.setAngemeldet(false);
 			session.setAttribute("angemeldeterMitarbeiter", m);
+
+			request.setAttribute("username", m.getLoginName());
+
+			log.info("User: " +  m.getLoginName() + " hat sich abgemeldet!");
+
+			RequestDispatcher rd = request.getRequestDispatcher("./index.jsp");
+			rd.forward(request, response);
+		} else
+		{
+			response.sendRedirect("./");
 		}
-		
-		request.setAttribute("username", m.getLoginName());
-		
-		
-		log.info("User: " +  m.getLoginName() + " hat sich abgemeldet!");
-		
-		RequestDispatcher rd = request.getRequestDispatcher("./index.jsp");
-		
-		rd.forward(request, response);
 	}
 }
